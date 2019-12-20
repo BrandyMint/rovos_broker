a501f464e3489ec65178d4d1a3f5ec96bb1deeaa
Danil Pismenny <danil@brandymint.ru>
2019-12-20 17:22:00 +0300

    minor

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,11 @@
 
 lock '3.11.2'
 
+set :application, 'rovos-broker'
+
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+
 set :user, 'wwwuser'
 set :repo_url, 'git@github.com:BrandyMint/iot_tcp_http_broker.git' if ENV['USE_LOCAL_REPO'].nil?
 set :keep_releases, 10
@@ -10,5 +15,14 @@ set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets]
 # set :config_files, fetch(:linked_files)
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:application)}" }
 ask :branch, ENV['BRANCH'] || proc { `git rev-parse --abbrev-ref HEAD`.chomp } if ENV['BRANCH']
-set :rbenv_type, :user
-set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :foreman_use_sudo, false # Set to :rbenv for rbenv sudo, :rvm for rvmsudo or true for normal sudo
+set :foreman_roles, :all
+set :foreman_init_system, 'upstart'
+set :foreman_export_path, ->{ File.join(Dir.home, '.init') }
+set :foreman_app, -> { fetch(:application) }
+set :foreman_app_name_systemd, -> { "#{ fetch(:foreman_app) }.target" }
+set :foreman_options, ->{ {
+  # app: application,
+  log: File.join(shared_path, 'log')
+} }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-set :application, 'rovos-broker'
 set :stage, :production
-set :rails_env, :production
-fetch(:default_env)[:rails_env] = :production
 
 server ENV['PRODUCTION_HOST'],
        user: fetch(:user),
