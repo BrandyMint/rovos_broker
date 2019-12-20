c6b653b93be2b3cc2d2faf18a460694de6e1a8b1
Danil Pismenny <danil@brandymint.ru>
2019-12-20 13:44:54 +0300

    rubocop

--- app/machines.rb
+++ app/machines.rb
@@ -9,48 +9,62 @@
 #
 module Machines
   HEADERS = { 'Content-Type' => 'application/json' }.freeze
+  # Сборник утилит
+  class Action
+    private
+
+    def fetch_connection(id)
+      connection = $tcp_server.connections.fetch id.to_i
+      yield connection
+    rescue KeyError
+      self.status = 404
+      self.body = { error: 'No such machine online' }.to_json
+    end
+  end
+
   # Список подключенных машин
-  class Index
+  class Index < Action
     include Hanami::Action
+
     def call(_params)
       headers.merge! HEADERS
+      self.status = 200
       self.body = { machines: $tcp_server.connections.keys }.to_json
     end
   end
 
   # Запрос на смену статуса машины
-  class ChangeStatus
+  class ChangeStatus < Action
     include Hanami::Action
+    # rubocop:disable Metrics/AbcSize
     def call(params)
       headers.merge! HEADERS
-      connection = $tcp_server.connections.fetch params[:id].to_i
-      sid = connection.channel.subscribe do |message|
-        @_env['async.callback'].call [200, HEADERS, { response_message: message.to_h }.to_json]
-        connection.channel.unsubscribe sid
+      fetch_connection params[:id] do |connection|
+        sent_message = connection.build_message state: params[:state].to_i, work_time: params[:work_time].to_i
+        sid = connection.channel.subscribe do |message|
+          @_env['async.callback'].call [201, HEADERS, { sent: sent_message.to_h, received: message.to_h }.to_json]
+          connection.channel.unsubscribe sid
+        end
+        connection.send_message sent_message
+        self.status = -1
       end
-      connection.send_message connection.build_message state: params[:state].to_i, work_time: params[:time].to_i
-      self.status = -1
-    rescue KeyError
-      self.status = 404
-      self.body = { error: 'No such machine online' }.to_json
     end
+    # rubocop:enable Metrics/AbcSize
   end
 
   # Получение статуса машины
-  class GetStatus
+  class GetStatus < Action
     include Hanami::Action
     def call(params)
-      connection = $tcp_server.connections.fetch params[:id].to_i
       headers.merge! HEADERS
-      self.status = 200
-      self.body = {
-        machine_id: connection.machine_id,
-        last_activity_at: connection.last_activity,
-        last_activity_elapsed: Time.now - connection.last_activity
-      }.to_json
-    rescue KeyError
-      self.status = 404
-      self.body = { error: 'No such machine online' }.to_json
+      fetch_connection params[:id] do |connection|
+        self.status = 200
+        self.body = {
+          machine_id: connection.machine_id,
+          last_activity_at: connection.last_activity,
+          last_activity_elapsed: Time.now - connection.last_activity
+        }.to_json
+      end
     end
   end
 end
